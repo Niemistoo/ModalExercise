09765100834e909a6e82a835d91df6515af32014
Niemistoo <jappe.nie@gmail.com>
2025-01-14 13:44:37 +0200

    Task2 completed

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => setModalVisible(previousState => !previousState);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Week 2</Text>
+        <Text style={styles.header}>Modal Exercise</Text>
+      </View>
+      <Pressable style={styles.showTextPressable} onPress={toggleModal}>
+        {modalVisible &&
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={toggleModal}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.casualText}>Modal message</Text>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={toggleModal}
+                >
+                  <Text style={styles.buttonText}>Close</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        }
+        <Text style={styles.casualText}>Show Modal</Text>
+      </Pressable>
+    </View >
   );
 }
 
@@ -16,5 +45,70 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  headerContainer: {
+    position: 'absolute',
+    top: 50, // Sijoittaa koko header-kontainerin ruudun yläreunaan
+    width: '100%',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 40,
+    marginBottom: 20,
+    alignContent: 'center',
+    textAlignVertical: 'center',
+    textDecorationLine: 'underline',
+  },
+  showTextPressable: {
+    padding: 40,
+    borderRadius: 20,
+    backgroundColor: 'orange',
+    elevation: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    paddingBottom: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  },
+  modalView: {
+    width: '100%',
+    backgroundColor: 'rgb(253, 253, 253)',
+    borderRadius: 20,
+    borderWidth: 1,
+    padding: 35,
+    alignItems: 'center',
+    textAlignVertical: 'center',
+    fontSize: 500,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  casualText: {
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    borderRadius: 20,
+    border: '1px solid black',
+    padding: 40,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: 'light-grey',
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
   },
 });
